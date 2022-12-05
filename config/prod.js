d876ef7612236c9266bb8640c4509331885c3c8b
Diego Mc <projkd1@gmail.com>
2022-12-05 17:35:14 +0200

    forcing push again

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  dbURL: 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
-  dbName : 'tester_db'
+  dbURL: 'mongodb+srv://wewix:wewix123321@wewix.azpvrit.mongodb.net/?retryWrites=true&w=majority',
+  dbName : 'wap_db'
 }
