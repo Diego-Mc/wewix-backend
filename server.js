5fa2902fc0be9461846938176590d5eecc4e2e4b
Diego Mc <projkd1@gmail.com>
2022-12-06 23:08:03 +0200

    build

--- server.js
+++ server.js
@@ -10,22 +10,26 @@ const http = require('http').createServer(app)
 app.use(cookieParser())
 app.use(express.json())
 
-
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')))
+  app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
-    const corsOptions = {
-        origin: ['http://127.0.0.1:5173', 'http://localhost:5173', 'http://127.0.0.1:3030', 'http://localhost:3030'],
-        credentials: true
-    }
-    app.use(cors(corsOptions))
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:5173',
+      'http://localhost:5173',
+      'http://127.0.0.1:3030',
+      'http://localhost:3030',
+    ],
+    credentials: true,
+  }
+  app.use(cors(corsOptions))
 }
 
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
-const userWapRoutes = require('./api/userWap/userWap.routes')
+// const userWapRoutes = require('./api/userWap/userWap.routes')
 const wapRoutes = require('./api/wap/wap.routes')
-const {setupSocketAPI} = require('./services/socket.service')
+const { setupSocketAPI } = require('./services/socket.service')
 
 // routes
 const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
@@ -33,7 +37,7 @@ const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
 
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
-app.use('/api/userWap', userWapRoutes)
+// app.use('/api/userWap', userWapRoutes)
 app.use('/api/wap', wapRoutes)
 setupSocketAPI(http)
 
@@ -41,12 +45,11 @@ setupSocketAPI(http)
 // so when requesting http://localhost:3030/index.html/wap/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue/react-router to take it from there
 app.get('/**', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
-
 
 const logger = require('./services/logger.service')
 const port = process.env.PORT || 3030
 http.listen(port, () => {
-    logger.info('Server is running on port: ' + port)
+  logger.info('Server is running on port: ' + port)
 })
